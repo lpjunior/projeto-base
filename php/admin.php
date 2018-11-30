d9dc0388af15b1701e36742d0f6e38e076be6312
Luis Paulo Lessa <prof.lpjunior@gmail.com>
2018-11-30 17:01:34 -0200

    Admin with RainTPL

--- php/admin.php
+++ php/admin.php
@@ -1,50 +1,34 @@
-<!doctype html>
-<html lang="pt-br">
-  <head>
-    <title>Projeto</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<?php
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
-    <link href="..\css\estilo.css" rel="stylesheet">
-  </head>
-  <body class="bg-color-home">
-    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#" style="color: orangered">Sistema Senac</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarText">
-        <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Registros</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Perfil</a>
-        </li>
-        </ul>
-        <span class="navbar-text">
-           <strong>Login efetuado às</strong> <em><?= ( (new DateTime("now", new DateTimeZone('America/Sao_Paulo')))->format('H:m:s')); ?></em>
-        </span>
-    </div>
-    </nav>
-    <header>
-    
-    </header>
-    <div class="container">
-        
-    </div>
+// include
+require ("../vendor/autoload.php");
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-  </body>
-</html>
+// namespace
+use Rain\Tpl;
+
+// config
+$config = array(
+    "tpl_dir"       => "templates/",
+    "cache_dir"     => "cache/",
+    "debug"         => false,
+);
+
+Tpl::configure( $config );
+
+
+// Add PathReplace plugin (necessary to load the CSS with path replace)
+Tpl::registerPlugin( new Tpl\Plugin\PathReplace() );
+
+
+// create the Tpl object
+$tpl = new Tpl;
+
+// assign a variable
+$tpl->assign( "HORA_LOGIN", ((new DateTime("now", new DateTimeZone('America/Sao_Paulo')))->format('H:m:s')));
+
+// assign an array
+#$tpl->assign( "week", array( "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" ) );
+
+// draw the template
+$tpl->draw( "admin" );
+?>
